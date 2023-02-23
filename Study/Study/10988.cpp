847d3f3f835a00d7dda0926db84bb216374b0294
my-Ahn <amy105071@gmail.com>
2023-02-23 20:39:40 +0900

    Update 10988.cpp

--- Study/Study/10988.cpp
+++ Study/Study/10988.cpp
@@ -5,51 +5,27 @@ int main()
 {
 	std::string str;
 	std::cin >> str;
-	char frontStr[100] = { 0 };
 	bool idx = false;
-	// Â¦¼ö
-	if (str.length() % 2 == 0)
+	int cnt = 0;
+	for (int i = 0; i < str.length() / 2; i++)
 	{
-		for (int i = 0; i < str.length() / 2; i++)
+		//str.length
+		if (str[i] == str[str.length() - i - 1])
 		{
-			frontStr[(str.length() / 2) - i-1] = str[i];			
+			cnt++;
 		}
-		for (int i = 0; i < str.length() / 2; i++)
-		{
-			if (frontStr[i] == str[i + (str.length() / 2)])
-			{
-				idx = true; // ÆÓ¸°µå·Ò
-			}
-			else
-			{
-				idx = false; 
-			}
-		}
+	}
+	/// <summary>
+	/// 2. str.length()/2
+	/// 5 -> str.length()/2 = 2 
+	/// </summary>
+	if (cnt == str.length() / 2)
+	{
+		std::cout << "1";
 	}
 	else
 	{
-		for (int i = 0; i < (str.length() / 2) ; i++)
-		{
-			frontStr[(str.length() / 2) - i - 1] = str[i];
-		}
-		for (int i = 0; i < str.length() / 2; i++)
-		{
-			if (frontStr[i] == str[i + 1 + (str.length() / 2)])
-			{
-				idx = true;
-			}
-			else
-			{
-				idx = false;
-			}
-		}
-	}
-	if (idx == true)
-	{
-		std::cout << 1;
-	}
-	else
-	{
-		std::cout << 0;
+		std::cout << "0";
 	}
 }
+		
